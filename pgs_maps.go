0a38a5fca4087bddd41f73368b1f5f1a61f96f28
Dmitrii Podkorytov <podkorytov@mail.ru>
2023-01-03 22:56:40 -0600

    Add files via upload

--- pgs_maps.go
+++ pgs_maps.go
@@ -1,6 +1,4 @@
 // All input from is strings 
-// use channels
-// r is accumulated state
 package main
 
 import (
@@ -16,9 +14,8 @@ import (
 
 var (
  	tracing      = flag.Bool("tracing", false, "Tracing")
- 	output_mask  = flag.String("output_mask","%v", "Output mask %v or %#v ")
- 	fold_mask    = flag.String("fold_mask","%v\ncall(%#v,%#v)", "Fold mask")
- 	result_mask  = flag.String("result_mask","%v", "Result mask")
+ 	output_mask  = flag.String("mask","%#v", "Output mask")
+ 	fold_mask    = flag.String("fold_mask","%v\ncall(%#v)", "Fold mask")
 
  	script       = flag.String("code","code.gmacro", "GoMacro code file")
 )
@@ -29,7 +26,7 @@ func main() {
 	flag.Parse()
 
 inputFile := *script 
-if *tracing  {log.Printf("Code file: %#v args=%#v mask=%#v \n",*script,flag.Args(),*output_mask)}
+if *tracing {log.Printf("Code file:%#v args=%v mask=%#v \n",*script,flag.Args(),*output_mask)}
 
 if len(os.Args)<2   {log.Printf("Run it:\n%v -code script_file.gomacro [-mask \"%v\"] [-tracing] < input_file\n",os.Args[0])
                      os.Exit(1) }
@@ -43,34 +40,39 @@ if len(os.Args)<2   {log.Printf("Run it:\n%v -code script_file.gomacro [-mask \"
    codeContent := string(code)
    // print code file content
    if *tracing  {log.Println("Code:\n",codeContent)}
-    r:=""
+
     scanner := bufio.NewScanner(os.Stdin)
     for scanner.Scan() {
-        r+=RunGomacroFolds(*tracing,codeContent,scanner.Text(),r) // Println will add back the final '\n'
+        fmt.Println(RunGomacroMaps(*tracing,codeContent,scanner.Text())) // Println will add back the final '\n'
     }
     if err := scanner.Err(); err != nil {
-        log.Println("Reading standard input:", err)
+        log.Println("reading standard input:", err)
     }
 
-    fmt.Printf(*result_mask,r)
+
 }
 
-func RunGomacroFolds(tracing bool,code string,toeval string,a string) string {
-    interp := fast.New()
-    r:=""
-    // slow call but is scripting
-    //vals, _ := interp.Eval(code+"\n"+"call(\""+toeval+"\")\n")
-    e:=fmt.Sprintf(*fold_mask,code,toeval,a)
-     if tracing { log.Printf("Eval(%#v) \n",e)}
-    vals, _ := interp.Eval(e)    
 /*
-if vals_err!=nil {log.Printf("Eval error %#v for code %#v\n",vals_err,e)
-                  //os.Exit(1)
-                 }
+var (
+ 	tracing      = flag.Bool("tracing", false, "Tracing")
+ 	output_mask  = flag.String("mask","%#v", "Output mask")
+ 	fold_mask    = flag.String("fold_mask","%v\ncall(%#v)", "Fold mask")
+
+ 	script       = flag.String("code","code.gmacro", "GoMacro code file")
+)
+
 */
+func RunGomacroMaps(tracing bool,code string,toeval string) string {
+    interp := fast.New()
+    // slow call but is scripting
+    //vals, _ := interp.Eval(code+"\n"+"call(\""+toeval+"\")\n")    
+    vals, _ := interp.Eval(fmt.Sprintf(*fold_mask,code,toeval))    
+
+    r:=""
+
 for i:=0;i<len(vals);i++ {
  if tracing { log.Printf("'%v'[%v] -> %#v \n",toeval,i,vals[i].ReflectValue())}
-            r+=fmt.Sprintf(*output_mask,vals[i].ReflectValue())// 
+            r+=fmt.Sprintf(*output_mask,vals[i].ReflectValue()) 
                          } 
  return r
 }
@@ -78,8 +80,11 @@ for i:=0;i<len(vals);i++ {
 func Gomacro_test() {
  interp  := fast.New()
  vals, err_vals := interp.Eval("func call(i int) int {\nreturn i}\ncall(1+2)\n")
- fmt.Printf("\nresult %v %#v\n",vals[0].ReflectValue(),  err_vals) 
+ //if *tracing {log.Printf("\nresult %v %#v\n",vals[0].ReflectValue(),  err_vals)} 
+ fmt.Printf("\nresult %#v \n",vals[0].ReflectValue()) 
+ if err_vals != nil {log.Printf("\nerror %#v\n", err_vals)} 
 
+// fmt.Printf("\nresult %v\n",RunGomacro("func call(i int) int {\n return i }\n","1+2")) 
 }
 
 
